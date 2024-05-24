97c5d9db1c4c199288551e1d9288b934df729578
Yurii Hetsko <getttzzz@gmail.com>
2024-05-24 21:40:05 +0200

    chore: add libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -35,32 +36,72 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        //keep version updated: https://www.jetpackcomposeversion.com/
+        kotlinCompilerExtensionVersion = "1.5.12"
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+            //Next two lines prevents this build fail
+            //Execution failed for task ':app:mergeDebugAndroidTestJavaResource'
+            excludes.add("META-INF/LICENSE.md")
+            excludes.add("META-INF/LICENSE-notice.md")
         }
+        jniLibs {
+            useLegacyPackaging = true // this is needed for Mockk library in order to work under androidTest
+        }
+    }
+
+    //If you use several KSP libraries (like Room), you can use this way of declaring generated sources:
+    applicationVariants.all {
+        addJavaSourceFoldersToModel(File(layout.buildDirectory.asFile.get(), "generated/ksp/$name/kotlin"))
     }
 }
 
 dependencies {
 
+    implementation(libs.material)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
+    //Coil
+    implementation(libs.coil.compose)
+
+    //DI
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
+    //Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.activity)
+    implementation(libs.androidx.compose.paging)
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    //Wonderful navigation
+    ksp(libs.compose.destinations.ksp)
+    implementation(libs.compose.destinations.animationes.core)
+
+    //Tests
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+    testImplementation(libs.kotlin.corutiens.test)
+
+    //Android Tests
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.mockk.android)
+
+    //Debugging
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- app/src/main/java/com/example/bearapp/MainActivity.kt
+++ app/src/main/java/com/example/bearapp/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             BearAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BearAppTheme {
-        Greeting("Android")
     }
 }
